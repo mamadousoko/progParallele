9ff721e858f5aef289f43c60d6184c8c12d10338
mamadousoko <mamadousoko@gmail.com>
2024-04-26 16:50:01 +0000

    commit two

--- ExoLab1.py
+++ ExoLab1.py
@@ -1,5 +1,6 @@
-def diff(x, y):
-    """ Calling function diff with arguments """
+def calc_distance(point1, point2):
+    """ Calling function calc_distance with arguments """
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2] - point2[2])**2)**0.5
 
 
 def my_map(func, arg1, arg2):
@@ -14,5 +15,10 @@ def my_map(func, arg1, arg2):
     for i, j in zip(arg1, arg2):
         res.append(func(i, j))
 
-    return res
+    return "{} \n {} \n and \n {}. \n {}".format(func.__doc__ , arg1, arg2, res)
 
+points1 = [(1.0, 1.0, 1.0, 1.0, 1.0), (2.0, 2.0, 2.0, 2.0, 2.0), (3.0, 3.0, 3.0, 3.0, 3.0)]
+points2 = [(4.0, 4.0, 4.0, 4.0, 4.0), (5.0, 5.0, 5.0, 5.0, 5.0), (6.0, 6.0, 6.0, 6.0, 6.0)]
+
+distances = my_map(calc_distance, points1, points2)
+print(distances)
